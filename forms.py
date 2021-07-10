f4af0cade26fd92139b8c3067f03badb5c6af419
Graham Billington <graahamhbillington@gmail.com>
2021-07-10 14:23:43 -0400

    corrected import statements

--- forms.py
+++ forms.py
@@ -1,8 +1,8 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from flask_wtf import Form
+from wtforms import BooleanField, StringField, PasswordField, SubmitField, validators
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
-class UserRegistrationForm(FlaskForm):
+class UserRegistrationForm(Form):
     username = StringField("Username",
                         validators = [DataRequired(), Length(min=2, max=20)]),
     email = StringField("Email", 
@@ -13,7 +13,7 @@ class UserRegistrationForm(FlaskForm):
                                     validators = [DataRequired(), EqualTo('password')])
     submit = SubmitField("Sign Up")
 
-class UserLoginForm(FlaskForm):
+class UserLoginForm(Form):
     email = StringField("Email", 
                         validators = [DataRequired(), Email()]),
     password = PasswordField("Password", 
